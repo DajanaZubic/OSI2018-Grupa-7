1b117e18f2e6b9e481ceb20818bcb51f48bafec3
NikolaCokic <45514806+NikolaCokic@users.noreply.github.com>
2018-12-31 05:42:48 +0100

    Add files via upload
    Utility zaglavlje sa funkcijom za racunanje novog stanja bodova na osnovu postojeceg. Prilikom testiranja, zaglavlje treba ukljuciti u projekat sa nekom od igara.

---
+++ utility.c
@@ -0,0 +1,19 @@
+#include "utility.h"
+#include <limits.h>
+#include <windows.h>
+
+int calcNTLPoints(int ukupniDobijeniBodovi, int ukupniIzgubljeniBodovi, int brojIgre)
+{
+    if(brojIgre*KOEFICIJENT_RASTA_ODNOSA<=1.4)
+    {
+        if((double)ukupniIzgubljeniBodovi / (double)ukupniDobijeniBodovi <= brojIgre*KOEFICIJENT_RASTA_ODNOSA)
+            return brojIgre*KOEFICIJENT_RASTA_ODNOSA*ukupniDobijeniBodovi-ukupniIzgubljeniBodovi;
+        else
+            return ukupniDobijeniBodovi-ukupniIzgubljeniBodovi/(brojIgre*KOEFICIJENT_RASTA_ODNOSA);
+    }
+    else if((double)ukupniIzgubljeniBodovi / (double)(ukupniDobijeniBodovi+180)<=1.4)
+    {
+        return 100;
+    }
+    return INT_MAX;
+}
---
+++ utility.h
@@ -0,0 +1,8 @@
+#ifndef UTILITY_H_INCLUDED
+#define UTILITY_H_INCLUDED
+#define KOEFICIJENT_RASTA_ODNOSA 0.2
+/*Funkcija calcNTLPoints radi na principu uporedjivanja trazenog odnosa ukupnog broja izgubljenih i dobijenih bodova sa
+trenutnim odnosom. Trazeni odnos raste linearno sa svakom igrom i iznosi KOEFICIJENT_RASTA_ODNOSA*brojIgre dok iznos ne bude 1.4*/
+int calcNTLPoints(int, int, int);      //calculate Next Turn Loosing Points
+
+#endif // UTILITY_H_INCLUDED
